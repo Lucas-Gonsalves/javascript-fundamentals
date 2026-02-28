b636ce839ba6d967b8d276eafcf606faf77880e3
Lucas Gonçalves <lucasluz1710@gmail.com>
2026-02-28 17:00:51 -0300

    feat(date-types): crate number example

---
+++ date-types/number/scripts.js
@@ -0,0 +1,14 @@
+// Number
+console.log(typeof 5);
+
+// integer positive
+console.log(5);
+
+// integer negative
+console.log(-5);
+
+// float
+console.log(7.5);
+
+// NAN - not a number
+console.log(12.5 / "rodrigo");
