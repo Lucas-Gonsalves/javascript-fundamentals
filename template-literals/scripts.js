c81f84fb25926309af7bddddfdd0496e0eff81b4
Lucas Gonçalves <lucasluz1710@gmail.com>
2026-02-28 16:54:52 -0300

    feat(template-literals): create example

---
+++ template-literals/scripts.js
@@ -0,0 +1,14 @@
+// Template literals (template strings, interpolation of strings)
+let username = "Lucas";
+let email = "lucas@gmail.com";
+
+// pass more than one parameter
+console.log(username, email);
+
+// concatenate a text
+let message = "Hello, " + username + ". You have been conected with the email: " + email;
+
+console.log(message);
+
+// template literals
+console.log(`Hello, ${username}. You have been conected with the email: ${email}`);
