e45022ec4dfca246bc6193c7722572e02a046b6e
Lucas Gonçalves <lucasluz1710@gmail.com>
2026-02-28 16:38:02 -0300

    feat(date-types): using string

---
+++ date-types/string/scripts.js
@@ -0,0 +1,14 @@
+// String
+let username = "Rodrigo";
+console.log(username);
+console.log(typeof username);
+
+console.log("a string with quotation marks");
+console.log("a string with apostrophe (single quotes)");
+
+// when use one of them
+console.log("a string with 'double quotes' inside of single quotes");
+
+console.log('a string with "single quotes" inside of double marks');
+
+console.log(`a string with gave accent allow to write multiples lines`)
