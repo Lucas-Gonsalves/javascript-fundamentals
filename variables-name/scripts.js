f91e328621b7bc6d6584d2e2c7ff85fb4ea09bbc
Lucas Gonçalves <lucasluz1710@gmail.com>
2026-02-28 12:13:42 -0300

    feat(variables-name): nomenclatures

---
+++ variables-name/scripts.js
@@ -0,0 +1,32 @@
+// case-sensitive
+let username = "lucas";
+let userName = "Jhon";
+
+console.log(username);
+console.log(username);
+
+// we can
+let $email = "lucasluz@gmail.com";
+let _email = "lucas@gmail.com";
+let user_email = "lucasluz@gmail.com";
+
+let Ação = "Cadastrar";
+
+console.log($email);
+console.log(_email);
+console.log(user_email);
+
+console.log(Ação);
+
+// we can't
+// let 1user = "lucas";
+
+// recomendation - camelCase
+let productName = "Keyboard";
+let fisrtName = "Lucas";
+let lastName = "Luz";
+
+// recomendation - snake_case (crawl)
+let product_name = "Keyboard";
+let fisrt_name = "Lucas";
+let last_name = "Luz";
