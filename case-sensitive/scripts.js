5c12c531cfc1f6928a5fad3e62c2e044c4d59c18
Lucas Gonçalves <lucasluz1710@gmail.com>
2026-02-28 10:46:01 -0300

    feat(case-sensitive): create example

---
+++ case-sensitive/scripts.js
@@ -0,0 +1,12 @@
+/**
+ * When a programming language is case-sensitive,
+ * it means that it distinguishes between uppercase and lowercase letters.
+ *
+ * For example: "Lucas" is considered different of "lucas"
+ */
+
+var name = "lucas";
+var Name = "Test";
+
+console.log(name);
+console.log(Name);
