e8f3306b67a900b081249a6a4553295f7ada9d34
Lucas Gonçalves <lucasluz1710@gmail.com>
2026-02-28 10:07:28 -0300

    feat(classroom): adding comments

--- classroom/scripts.js
+++ classroom/scripts.js
@@ -1,1 +1,14 @@
-console.log("Hello World!")
+// Displaying "Hello World!" in console
+console.log("Hello World!"); // Side comment
+
+/**
+ * Multi-line comment
+ *
+ * Comments are snippets of text that can be added to the code.
+ *
+ * Why use comments during your studies?:
+ * - Provide additional information about what the code does;
+ * - Leave reminders of things to do;
+ * - Remind future readers what it does;
+ * - Etc.
+ */
