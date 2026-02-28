10cc2e8043853dde2d7a65eb5b3dc8a2f0b226bd
Lucas Gonçalves <lucasluz1710@gmail.com>
2026-02-28 10:57:25 -0300

    feat(variables): using const

---
+++ variables/const/scripts.js
@@ -0,0 +1,7 @@
+// A constant creates a variable whose value is fixed (cannot be changed).
+const number = 42;
+console.log(number);
+
+// This will generate an error because the value cannot be changed.
+number = 55;
+console.log(number);
