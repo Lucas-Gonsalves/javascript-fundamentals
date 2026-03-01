b20b508f374207359c8b110011776ddee79905be
Lucas Gonçalves <lucasluz1710@gmail.com>
2026-02-28 22:13:44 -0300

    feat(expressions-and-operators): create equal-difference

---
+++ expressions-and-operators/equal-different/scripts.js
@@ -0,0 +1,14 @@
+let one = 1;
+let two = 2;
+
+// == equal to
+console.log("Equal to:");
+console.log(one == two); // ...false
+console.log(one == 1); // ...true
+console.log(one == "1"); // ...true
+
+// != different to
+console.log("Difference to:");
+console.log(one != two); // ...true
+console.log(one != 1); // ...false
+console.log(one != "1"); // ...false
