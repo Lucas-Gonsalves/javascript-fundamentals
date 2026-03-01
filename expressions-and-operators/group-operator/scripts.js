869c113604718463e7ae3b1188a822c8d9e04c4e
Lucas Gonçalves <lucasluz1710@gmail.com>
2026-02-28 22:06:54 -0300

    feat(expressions-and-operators): create group operator

---
+++ expressions-and-operators/group-operator/scripts.js
@@ -0,0 +1,13 @@
+// Grouping operator (precedence order)
+
+let total = 2 + 3 * 4;
+console.log(total); // ...14
+
+let newTotal = (2 + 3) * 4;
+console.log(newTotal); // ...20
+
+let avarege = 9.5 + 7 + 5 / 3;
+console.log(avarege)
+
+let newAvarege = (9.5 + 7 + 5) / 3;
+console.log(newAvarege)
