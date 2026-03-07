12324fb9e941c52d1d2924bd2f1d988fe75c4355
Lucas Gonçalves <lucasluz1710@gmail.com>
2026-03-07 16:27:48 -0300

    feat(DOM): create query selector

---
+++ DOM/query-selector/scripts.js
@@ -0,0 +1,7 @@
+// get elements by query ID
+const getByQueryId = document.querySelector("#guest-1");
+console.dir(getByQueryId);
+
+// get elements by query class
+const getByQueryClass = document.querySelectorAll(".guest");
+console.dir(getByQueryClass);
