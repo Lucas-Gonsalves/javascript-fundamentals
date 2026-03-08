5aad0bc7121abdbedf344b37e57afbb31eaa8a16
Lucas Gonçalves <lucasluz1710@gmail.com>
2026-03-08 20:50:37 -0300

    feat(DOM): create manipulate-attributes

---
+++ DOM/manipulate-attributes/scripts.js
@@ -0,0 +1,8 @@
+const input = document.querySelector("input");
+
+// add/update a new attribute
+input.setAttribute("disabled", true);
+input.setAttribute("type", "file");
+
+// remove an attribute
+input.removeAttribute("id");
