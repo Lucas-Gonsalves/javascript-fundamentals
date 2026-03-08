8ece20cbdd8f0ef35b954c0b3b51185429663f1d
Lucas Gonçalves <lucasluz1710@gmail.com>
2026-03-08 20:30:16 -0300

    feat(DOM): create change-styles

---
+++ DOM/change-styles/scripts.js
@@ -0,0 +1,14 @@
+// const input = document.querySelector("name");
+
+// add new class to a element
+// input.classList.add("input-error");
+
+// remove class to a element
+// input.classList.remove("input-error");
+
+// toggle an existing class
+// input.classList.toggle("input-error");
+
+// styling an element
+const button = document.querySelector("button");
+button.style.backgroundColor = "red";
