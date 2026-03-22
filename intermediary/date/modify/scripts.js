98abe5ac20f69aa849352c7655757e75c6c8a52e
Lucas Gonçalves <lucasluz1710@gmail.com>
2026-03-22 20:54:09 -0300

    feat(intermediary): date create modify

---
+++ intermediary/date/modify/scripts.js
@@ -0,0 +1,36 @@
+let date = new Date("July 3, 2024 14:30:00")
+
+// year modify
+date.setFullYear(2030)
+
+console.log(date) // showing results
+
+
+// month modify
+date.setMonth(7) // June
+
+console.log(date) // showing results
+
+
+// day modify
+date.setDate(10)
+
+console.log(date) // showing results
+
+
+// hour modify
+date.setHours(18)
+
+console.log(date) // showing results
+
+
+// Minutes modify
+date.setMinutes(15)
+
+console.log(date) // showing results
+
+
+// Seconds modify
+date.setSeconds(1)
+
+console.log(date) // showing results
