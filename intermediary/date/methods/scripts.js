b7fdec58fc3c6fd5b650d1a8890dd810cab93890
Lucas Gonçalves <lucasluz1710@gmail.com>
2026-03-22 20:46:17 -0300

    feat(intermediary): date create methods

---
+++ intermediary/date/methods/scripts.js
@@ -0,0 +1,18 @@
+let date = new Date("2024-07-02T14:30:10")
+console.log(date) // normal date
+
+//return index of one of this
+
+console.log(date.getDay()) // getting day week, 0-6, (sunday is 0)
+
+console.log(date.getDate()) // get month day (0-30 = 1-31)
+
+console.log(date.getMonth()) // get month (0-11)
+
+// full
+console.log(date.getFullYear)
+
+// hours
+console.log(date.getHours())
+console.log(date.getMinutes())
+console.log(date.getSeconds())
