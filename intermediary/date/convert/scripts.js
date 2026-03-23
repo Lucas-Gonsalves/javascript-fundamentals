758f85cc8ef84aec7228a4694d2fda193d089fef
Lucas Gonçalves <lucasluz1710@gmail.com>
2026-03-22 21:17:16 -0300

    feat(intermediary): date create display-formated

--- intermediary/date/convert/scripts.js
+++ intermediary/date/convert/scripts.js
@@ -1,8 +1,9 @@
 let date = new Date("2024-07-02T14:30:10")
 
-console.log(date.toString()) // converts to string
+console.lod(date.toLocaleDateString()) // show only date on 'dd/mm/yyyy' format
 
-console.log(date.toDateString()) // converts to a date string
+console.lod(date.toLocaleTimeString()) // show only time on 'hh/mm/ss' format
 
-// return hours only
-console.log(date.toTimeString())
+// display date and hour in choosen format
+console.log(date.toLocaleDateString("en")) // show only date on 'mm/dd/yyyy' format
+console.log(date.toLocaleTimeString("en")) // show only date on 'hh(0-12)/mm/ss' PM/AM  format
