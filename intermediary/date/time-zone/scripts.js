f1fbba964b759acd7b99e7a3303d7a55c9a44897
Lucas Gonçalves <lucasluz1710@gmail.com>
2026-03-22 22:13:57 -0300

    feat(intermediary): date create time-zone

---
+++ intermediary/date/time-zone/scripts.js
@@ -0,0 +1,5 @@
+const dateWithTimeZone = new Date("2023-12-13T22:56:30.603+03:00")
+// console.log(dateWithTimeZone.toISOString())
+
+console.log(new Date().toLocaleDateString())
+console.log(dateWithTimeZone.toLocaleDateString())
