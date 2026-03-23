7d2c5a94087b314fce60e85761cb30d044245bf1
Lucas Gonçalves <lucasluz1710@gmail.com>
2026-03-22 21:10:46 -0300

    feat(intermediary): date create convert

---
+++ intermediary/date/convert/scripts.js
@@ -0,0 +1,8 @@
+let date = new Date("2024-07-02T14:30:10")
+
+console.log(date.toString()) // converts to string
+
+console.log(date.toDateString()) // converts to a date string
+
+// return hours only
+console.log(date.toTimeString())
