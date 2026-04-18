86072cf8561b86f988863c0e9473f1fcec5c6c1c
Lucas Gonçalves <lucasluz1710@gmail.com>
2026-04-18 13:02:47 -0300

    feat(class): create prototype

---
+++ intermediary/class/prototype.js
@@ -0,0 +1,12 @@
+const address = {
+  city: "São Paulo",
+  country: "Brazil"
+}
+console.log(address)
+
+const users = ["Lucas", "Anna", "Gabriel"]
+console.log(users)
+
+
+const text = "This is a text just to se the prototype"
+console.log(text.__proto__)
